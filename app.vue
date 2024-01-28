1d6f2f792188344590acd40fc47b86cb83bc06fb
Steven Park <steven.j.park@outlook.com>
2024-01-28 18:35:45 -0500

    Main content

--- app.vue
+++ app.vue
@@ -1,7 +1,49 @@
 <template>
-  <div>
+  <div class="card">
     <img src="./assets/images/image-qr-code.png">
     <h3>Improve your front-end skills by building projects</h3>
-    <p>Scan the QR code to visit Frontend Mentor and take your coding skils to the next level</p>
+    <h4>Scan the QR code to visit Frontend Mentor and take your coding skils to the next level</h4>
   </div>
 </template>
+
+<style lang="scss">
+@use "~/assets/scss/main";
+
+$text-header: hsl(218, 44%, 22%);
+$text-accent: hsl(220, 15%, 55%);
+$card-bg: hsl(0, 0%, 100%);
+
+h3, h4 {
+  text-align: center;
+}
+
+h3 {
+  font-weight: 700;
+  color: $text-header;
+  margin-top: 25px;
+  margin-bottom: 15px;
+}
+
+h4 {
+  font-weight: 400;
+  color: $text-accent;
+  margin-bottom: 25px;
+}
+
+.card {
+  background-color: $card-bg;
+  width: 325px;
+  margin: 15vh auto;
+  padding: 15px;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    width: 100%;
+    border-radius: inherit;
+  }
+}
+</style>
