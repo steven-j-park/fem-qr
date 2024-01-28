e6cf7ec2f7f1be9b1eb04e989862194adfcf6c4f
Steven Park <steven.j.park@outlook.com>
2024-01-28 17:45:34 -0500

    Basic content structure

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <img src="./assets/images/image-qr-code.png">
+    <h3>Improve your front-end skills by building projects</h3>
+    <p>Scan the QR code to visit Frontend Mentor and take your coding skils to the next level</p>
   </div>
 </template>
